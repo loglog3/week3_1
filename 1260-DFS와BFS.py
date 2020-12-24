d55e1ed6ebcecbc0ac57666554bdf24e1e367ec8
karockai <75655052+karockai@users.noreply.github.com>
2020-12-24 19:52:57 +0900

    d

---
+++ 1260-DFS와BFS.py
@@ -0,0 +1,30 @@
+import sys
+import copy
+from collections import deque 
+
+
+sys.stdin = open("input.txt", "r")
+
+# def dfs(V):
+#     print(v, end=' ')
+#     visit[v] = 1
+#     for i in range(1, n+1):
+#         if visit[i] == 0
+
+
+
+N = 5
+
+# N, M, V = map(int,sys.stdin.readline().split())
+
+graph = {i:[] for i in range(1, N+1)}
+print(graph)
+# for i in range(M):
+#     x, y = map(int,sys.stdin.readline().split())
+#     graph[x].append(y)
+#     graph[y].append(x)
+
+
+    
+
+
